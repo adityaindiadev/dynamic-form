189b4d06bbcf6ec0bc4b1466f7e5ca11aea0d7f3
“adityaindiadev” <aditya.india.dev@gmail.com>
2024-04-15 19:25:25 +0530

    shadcn implemented

--- src/components/Modules/Expenses/AddExpense.tsx
+++ src/components/Modules/Expenses/AddExpense.tsx
@@ -8,6 +8,9 @@ import ApiCaller from "@/app/utils/ApiCaller/ApiCaller";
 import Constants from "@/app/utils/Constants";
 import Image from "next/image";
 import { useEffect, useState } from "react";
+import { Button } from "@/components/ui/button"
+import { ComboboxDemo } from "@/components/ui/ComboBox";
+
 
 interface SelectedExpenseCategoryProps {
 
@@ -38,6 +41,8 @@ export default function AddExpense() {
     const [expenseCategories, setExpenseCategories] = useState<any>([]);
 
     const [showModal, setShowModal] = useState(false);
+
+    const [isShowAdvanceAmount, setIsShowAdvanceAmount] = useState(false)
 
 
     useEffect(() => {
@@ -86,6 +91,7 @@ export default function AddExpense() {
         let responseJson2 = await ApiCaller.getData(endPoint)
 
         console.log("advanceExpenseData", responseJson2);
+        setIsShowAdvanceAmount(responseJson2?.shouldAdvanceAmountFlowToExpense)
         // setshowadvanceAmount(responseJson2.shouldAdvanceAmountFlowToExpense)
 
     }
@@ -95,13 +101,12 @@ export default function AddExpense() {
             <Loader isLoading={isLoading} />
             <main className="flex min-h-screen flex-col items-center  p-15">
                 <TextInput title="Expense Title" placeholder="Enter Expense" />
-                <TextInput title="Advance" placeholder="Enter Advance" />
-                <button type="submit" className="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                    <svg className="me-1 -ms-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clipRule="evenodd"></path></svg>
-                    Add new product
-                </button>
+               {isShowAdvanceAmount && <TextInput title="Advance" placeholder="Enter Advance" /> }
+                <Button>Click me</Button>
+
                 <DropDown itemArr={expenseCategories} value={(item) => item?.expenseName} keyExtractor={(item, index) => String(index)} onSelect={(item) => setSelectedExpenseCategory(item)} selectedValue={selectedExpenseCategory.expenseName} />
 
+            <ComboboxDemo/>
                
 
 
